1915e9d3d2db3534103862b4a48288b34595cae1
jtfalkenstein <jtfalkenstein@gmail.com>
2016-05-24 12:29:28 -0500

    Added CardDTOFactorTests file and laid out some plans for furture.

---
+++ FaithEngage.Core.Tests/CardTests/CardDtoFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FaithEngage.Core.Tests
+{
+	[TestFixture]
+	public class CardDtoFactoryTests
+	{
+		
+		[Test]
+		public void GetCards_ValidDict_ReturnsCardDTOs()
+		{
+		}
+
+		[Test]
+		public void GetCards_EmptyDict_ReturnsEmptyArray()
+		{
+		}
+
+		[Test]
+		public void GetCards_EncountersException_SkipsCardAndContinues()
+		{
+		}
+	}
+}
+
