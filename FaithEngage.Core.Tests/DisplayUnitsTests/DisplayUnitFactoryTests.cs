a5ca2000824e2d427c0abb48e9c48ac0bffa4db9
jtfalkenstein <jtfalkenstein@gmail.com>
2016-05-26 15:09:28 -0500

    Created dummy display unit and replace that for the fake. Fixed the test.

--- FaithEngage.Core.Tests/DisplayUnitsTests/DisplayUnitFactoryTests.cs
+++ FaithEngage.Core.Tests/DisplayUnitsTests/DisplayUnitFactoryTests.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using FaithEngage.Core.DisplayUnits.Interfaces;
 using FaithEngage.Core.Exceptions;
-using FaithEngage.Plugins.DisplayUnits.TextUnitPlugin;
 using FakeItEasy;
 using NUnit.Framework;
 using FaithEngage.Core.PluginManagers.DisplayUnitPlugins;
 using FaithEngage.Core.DisplayUnits;
+using System.Reflection;
 
 namespace FaithEngage.Core.DisplayUnits
 {
@@ -15,18 +15,46 @@ namespace FaithEngage.Core.DisplayUnits
     {
         public const string VALID_STRING = "VALID STRING";
         public const string INVALID_STRING = "INVALID STRING";
-        private readonly IDisplayUnitPluginContainer _mgr = A.Fake<IDisplayUnitPluginContainer>();
+		private readonly IDisplayUnitPluginContainer _container = A.Fake<IDisplayUnitPluginContainer>();
         private readonly DisplayUnitPlugin _plgin = A.Fake<DisplayUnitPlugin> ();
         private DisplayUnitDTO _validDtoNoId;
         private DisplayUnitDTO _validDtoWithId;
         private DisplayUnitDTO _invalidDto;
-        private DisplayUnit _unit;
         private Guid _guid;
 
-        [TestFixtureSetUp]
+		private class DummyDisplayUnit : DisplayUnit
+		{
+			public DummyDisplayUnit (Dictionary<string, string> attributes) : base (attributes)
+			{
+			}
+			public DummyDisplayUnit (Guid id, Dictionary<string, string> attributes) : base (id, attributes)
+			{
+			}
+			public override Dictionary<string, string> GetAttributes ()
+			{
+				throw new NotImplementedException ();
+			}
+
+			public override FaithEngage.Core.Cards.Interfaces.IRenderableCard GetCard ()
+			{
+				throw new NotImplementedException ();
+			}
+
+			public override void SetAttributes (Dictionary<string, string> attributes)
+			{
+				throw new NotImplementedException ();
+			}
+
+			public override DisplayUnitPlugin Plugin {
+				get {
+					throw new NotImplementedException ();
+				}
+			}
+		}
+			
+		[TestFixtureSetUp]
         public void init()
         {
-            _unit = A.Fake<DisplayUnit> ();
             _guid = Guid.NewGuid ();
 
             _validDtoWithId = new DisplayUnitDTO (_guid, _guid);
@@ -46,20 +74,19 @@ namespace FaithEngage.Core.DisplayUnits
             _invalidDto.Attributes = new Dictionary<string,string> ();
 
 
-            A.CallTo (() => _mgr.Resolve (VALID_STRING)).Returns (_plgin);
-            A.CallTo (() => _mgr.Resolve (INVALID_STRING)).Returns (null);
-            A.CallTo (() => _plgin.DisplayUnitType).Returns (typeof(TextUnit));
-
+            A.CallTo (() => _container.Resolve (VALID_STRING)).Returns (_plgin);
+            A.CallTo (() => _container.Resolve (INVALID_STRING)).Returns (null);
+			A.CallTo (() => _plgin.DisplayUnitType).Returns (typeof(DummyDisplayUnit));
         }
 
         [Test]
         public void InstantiateNew_ValidPluginID_EmptyDict_ReturnsCorrectDisplayUnit()
         {
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
 
             var du = factory.InstantiateNew (VALID_STRING, new Dictionary<string, string> ());
 
-            Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
+			Assert.That (du, Is.InstanceOf(typeof(DummyDisplayUnit)));
         }
 
         [Test]
@@ -70,10 +97,10 @@ namespace FaithEngage.Core.DisplayUnits
                 { "PositionInEvent", "1" },
                 { "DateCreated", DateTime.Now.ToShortDateString() }
             };
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
             var du = factory.InstantiateNew (VALID_STRING, dict);
 
-            Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
+            //Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
             Assert.That (du.PositionInEvent == 1);
             Assert.That (du.DateCreated == DateTime.Now.Date);
         }
@@ -82,17 +109,17 @@ namespace FaithEngage.Core.DisplayUnits
         [ExpectedException(typeof(NotRegisteredPluginException))]
         public void InstantiateNew_InvalidPluginId_ThrowsException()
         {
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
             var du = factory.InstantiateNew (INVALID_STRING, new Dictionary<string, string> ());
         }
 
         [Test]
         public void ConvertFromDto_ValidDtoWithoutId_NewDisplayUnit()
         {
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
             var du = factory.ConvertFromDto (_validDtoNoId);
 
-            Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
+            //Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
             Assert.That (du.Id != Guid.Empty);
             Assert.That (du.PositionInEvent == 1);
             Assert.That (du.GetAttribute ("Text") == VALID_STRING);
@@ -101,10 +128,10 @@ namespace FaithEngage.Core.DisplayUnits
         [Test]
         public void ConvertFromDto_ValidDtoWithId_NewDisplayUnit()
         {
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
             var du = factory.ConvertFromDto (_validDtoWithId);
 
-            Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
+            //Assert.That (du, Is.InstanceOf (typeof(TextUnit)));
             Assert.That (du.Id == _guid);
             Assert.That (du.PositionInEvent == 1);
             Assert.That (du.GetAttribute ("Text") == VALID_STRING);
@@ -113,7 +140,7 @@ namespace FaithEngage.Core.DisplayUnits
         [Test]
         public void ConvertFromDto_InvalidDTOPlugin_ReturnsNull()
         {
-            var factory = new DisplayUnitFactory (_mgr);
+            var factory = new DisplayUnitFactory (_container);
             var du = factory.ConvertFromDto (_invalidDto);
             Assert.That (du, Is.Null);
         }
