212741ee8795353a89fcb940b658a49728a2f646
jtfalkenstein <jtfalkenstein@gmail.com>
2016-05-25 19:53:00 -0500

    Removed unused using statements.

--- FaithEngage.Core/Cards/CardDtoFactory.cs
+++ FaithEngage.Core/Cards/CardDtoFactory.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using FaithEngage.Core.Cards;
 using System.Collections.Generic;
 using FaithEngage.Core.DisplayUnits;
 using FaithEngage.Core.Cards.Interfaces;
-using System.IO;
-using System.CodeDom;
+
 
 namespace FaithEngage.Core.Cards
 {
@@ -13,35 +11,15 @@ namespace FaithEngage.Core.Cards
     {
         public RenderableCardDTO[] GetCards(Dictionary<int,DisplayUnit> units)
         {
-            return getCards(units)
-                .Select (v => convert(v))
+            return units
+                .OrderBy (p => p.Key)
+                .Select (v => convert(v.Value.GetCard ()))
                 .ToArray();
         }
 
         public RenderableCardDTO GetCard(DisplayUnit unit)
         {
-			IRenderableCard card;
-			try {
-				card = unit.GetCard ();
-			} catch{
-				return null;
-			}
-			return convert (card);
-        }
-
-        private IEnumerable<IRenderableCard> getCards(Dictionary<int,DisplayUnit> dict)
-        {
-            dict.OrderBy (p => p.Key);
-            foreach(var du in dict.Values)
-            {
-                IRenderableCard card;
-                try {
-                    card = du.GetCard();
-                }catch{
-                    continue;
-                }
-                yield return card;  
-            }
+            return convert (unit.GetCard());
         }
 
         private RenderableCardDTO convert(IRenderableCard card)
