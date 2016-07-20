84b41da00190627f6f8efb5cdcb1fe3542461a10
jtfalkenstein <jtfalkenstein@gmail.com>
2016-07-20 12:36:14 -0500

    Added methods for plugin manager and IPluginManager

--- FaithEngage.Core/PluginManagers/Interfaces/IPluginManager.cs
+++ FaithEngage.Core/PluginManagers/Interfaces/IPluginManager.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO.Compression;
+
 namespace FaithEngage.Core.PluginManagers.Interfaces
 {
     public interface IPluginManager
     {
+		Guid Install(ZipArchive zipFile);
+		void Uninstall(Guid pluginId);
     }
 }
 
--- FaithEngage.Core/PluginManagers/PluginManager.cs
+++ FaithEngage.Core/PluginManagers/PluginManager.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.IO.Compression;
+using FaithEngage.Core.PluginManagers.Files.Interfaces;
 using FaithEngage.Core.PluginManagers.Interfaces;
 
 namespace FaithEngage.Core.PluginManagers
 {
-    public class PluginManager : IPluginManager
-    {
-        
-    }
+	public class PluginManager : IPluginManager
+	{
+		public PluginManager(IPluginFileManager fileManager)
+		{
+
+		}
+
+		public Guid Install(ZipArchive zipFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Uninstall(Guid pluginId)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
 
