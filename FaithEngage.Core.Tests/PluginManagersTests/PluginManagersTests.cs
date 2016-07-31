80be54a12b85b79d18e0bee52d7cef982f1f14d6
jtfalkenstein <jtfalkenstein@gmail.com>
2016-07-31 11:34:22 -0500

    Deleted unused project.  Addd uninstall test.

--- FaithEngage.Core.Tests/PluginManagersTests/PluginManagersTests.cs
+++ FaithEngage.Core.Tests/PluginManagersTests/PluginManagersTests.cs
@@ -54,12 +54,58 @@ namespace FaithEngage.Core.PluginManagers
 
 			A.CallTo(() => _fileMgr.GetFilesForPlugin(A<Guid>.Ignored)).Returns(pluginFiles);
 
-			_pluginMgr.Install(zipFile);
+			var num = _pluginMgr.Install(zipFile);
 
             zipFile.Dispose ();
 			A.CallTo(() => _fileMgr.StoreFilesForPlugin(fileslist, key, true)).MustHaveHappened();
+            A.CallTo (() => _fileMgr.FlushTempFolder (key)).MustHaveHappened();
 			A.CallTo(() => _mgr.RegisterNew(A<Plugin>.Ignored)).MustHaveHappened();
+            Assert.That (num == 1);
+        }
 
+        [Test]
+        public void Install_ZipFileHasNoDlls_DoesNotInstall_ReturnsZero()
+        {
+            var zipFile = ZipFile.OpenRead (Path.Combine ("TestingFiles", "pluginZip.zip"));
+            var zipDir = new DirectoryInfo (Path.Combine (Directory.GetCurrentDirectory (), "TestingFiles", "pluginZip"));
+            Guid key = Guid.Empty;
+            var files = zipDir.GetFiles ().Where(p=> p.Extension != ".dll");
+            var fileslist = files.ToList ();
+            A.CallTo (() => _fileMgr.ExtractZipToTempFolder (zipFile, A<Guid>.Ignored))
+             .ReturnsLazily ((ZipArchive z, Guid k) => {
+                 key = k;
+                 return fileslist;
+             });
+
+            var fileId = Guid.NewGuid ();
+            var pluginFiles = files.ToDictionary (p => {
+                fileId = Guid.NewGuid ();
+                return fileId;
+            }, p => {
+                var pfile = new PluginFileInfo (key, p);
+                pfile.FileId = Guid.NewGuid ();
+                return pfile;
+            });
+
+            A.CallTo (() => _fileMgr.GetFilesForPlugin (A<Guid>.Ignored)).Returns (pluginFiles);
+
+            var num = _pluginMgr.Install (zipFile);
+
+            zipFile.Dispose ();
+
+            A.CallTo (() => _fileMgr.StoreFilesForPlugin (fileslist, key, true)).MustHaveHappened ();
+            A.CallTo (() => _fileMgr.FlushTempFolder (key)).MustHaveHappened ();
+            A.CallTo (() => _mgr.RegisterNew (A<Plugin>.Ignored)).MustNotHaveHappened();
+            Assert.That(num == 0);
+        }
+
+        [Test]
+        public void Uninstall_CallsUninstall()
+        {
+            var key = Guid.NewGuid ();
+            _pluginMgr.Uninstall(key);
+
+            A.CallTo (() => _mgr.UninstallPlugin (key)).MustHaveHappened();
         }
     }
 }
