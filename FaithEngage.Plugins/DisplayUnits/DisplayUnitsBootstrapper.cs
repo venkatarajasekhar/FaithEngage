7c6a492e6d72e6d8c4d8bae9870adf51d3be9a35
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-16 14:06:17 -0500

    Making plugins use bootstrapper vs. dll load.

---
+++ FaithEngage.Plugins/DisplayUnits/DisplayUnitsBootstrapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using FaithEngage.Core.Bootstrappers;
+using FaithEngage.Core.Containers;
+using FaithEngage.Core.Factories;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FaithEngage.CorePlugins.DisplayUnits
+{
+	public class DisplayUnitsBootstrapper : IBootstrapper
+	{
+		public BootPriority BootPriority
+		{
+			get
+			{
+				return BootPriority.Normal;
+			}
+		}
+
+		public void Execute(IAppFactory factory)
+		{
+			registerTemplates(factory);
+		}
+
+		private void registerTemplates(IAppFactory factory)
+		{
+			var tempService = factory.TemplatingService;
+			var curretDir = AppDomain.CurrentDomain.BaseDirectory;
+			var dir = new DirectoryInfo(Path.Combine(curretDir, "Plugin Files"));
+			var files = dir.EnumerateFiles("*", SearchOption.AllDirectories);
+			var filesNeeded = new[]
+			{
+				new{fname = "TextUnitEditorTemplate.cshtml", name = "TextUnitEditor"},
+				new{fname = "TextUnitCardTemplate.cshtml", name="TextUnitCard" }
+			};
+			var filesObtained =
+				from f in files
+				from fn in filesNeeded
+				where f.FullName.Contains(fn.fname)
+				select new
+				{
+					fileInfo = f,
+					key = fn.name
+				};
+
+			foreach (var file in filesObtained)
+			{
+				try
+				{
+					var tempString = getTemplateString(file.fileInfo);
+					tempService.RegisterTemplate(tempString, file.key);
+				}
+				catch (Exception ex)
+				{
+					continue;
+				}
+			}
+
+		}
+
+		private string getTemplateString(FileInfo file)
+		{
+			string templateString = null;
+			using (var reader = file.OpenText())
+			{
+				templateString = reader.ReadToEnd();
+			}
+			return templateString;
+		}
+
+		public void LoadBootstrappers(IBootList bootstrappers)
+		{
+		}
+
+		public void RegisterDependencies(IRegistrationService regService)
+		{
+		}
+	}
+}
---
+++ FaithEngage.Plugins/RazorTemplating/RazorTemplatingBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FaithEngage.Core.Bootstrappers;
+using FaithEngage.Core.Containers;
+using FaithEngage.Core.Factories;
+using FaithEngage.Core.TemplatingService;
+
+namespace FaithEngage.CorePlugins.RazorTemplating
+{
+	public class RazorTemplatingBootstrapper : IBootstrapper
+	{
+		public BootPriority BootPriority
+		{
+			get
+			{
+				return BootPriority.First;
+			}
+		}
+
+		public void Execute(IAppFactory factory)
+		{
+		}
+
+		public void LoadBootstrappers(IBootList bootstrappers)
+		{
+		}
+
+		public void RegisterDependencies(IRegistrationService regService)
+		{
+			regService.Register<ITemplatingService, RazorTemplatingService>(LifeCycle.Singleton);
+		}
+	}
+}
