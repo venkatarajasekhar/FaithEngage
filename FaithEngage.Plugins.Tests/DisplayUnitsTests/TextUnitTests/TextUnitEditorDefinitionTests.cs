a5f9a0d2d72e15907747be9aa1f73265b8d18525
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-19 14:46:08 -0500

    Adding tests for TextUnitEditorDefinition

--- FaithEngage.Plugins.Tests/DisplayUnitsTests/TextUnitTests/TextUnitEditorDefinitionTests.cs
+++ FaithEngage.Plugins.Tests/DisplayUnitsTests/TextUnitTests/TextUnitEditorDefinitionTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using FaithEngage.Core.DisplayUnitEditor;
+using FaithEngage.Core.TemplatingService;
+using FakeItEasy;
 using NUnit.Framework;
 namespace FaithEngage.CorePlugins.DisplayUnits.TextUnit
 {
@@ -7,13 +10,13 @@ namespace FaithEngage.CorePlugins.DisplayUnits.TextUnit
         [Test]
         public void GetHtmlEditorForm_CompilesEditorFromTemplate()
         {
-            Assert.Inconclusive ("No test yet.");
-        }
+			var tService = A.Fake<ITemplatingService>();
+			var context = new DisplayUnitEditorContext();
+			var tuEditDef = new TextUnitEditorDefinition();
 
-        [Test]
-        public void GetHtmlEditorForm_TemplatingServiceThrowsException_Throws()
-        {
-            Assert.Inconclusive ("No test yet.");
+			tuEditDef.GetHtmlEditorForm(tService, context);
+
+			A.CallTo(()=> tService.CompileHtmlFromTemplateKey("TextUnitEditor", context)).MustHaveHappened();
         }
     }
 }
