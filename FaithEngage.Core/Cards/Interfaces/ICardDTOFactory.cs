d0e49ffab3af14a3d0331ccfb9189277a55b677c
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-20 13:18:09 -0500

    documenting ICardDTOFactory

--- FaithEngage.Core/Cards/Interfaces/ICardDTOFactory.cs
+++ FaithEngage.Core/Cards/Interfaces/ICardDTOFactory.cs
@@ -5,11 +5,31 @@ using FaithEngage.Core.DisplayUnits;
 
 namespace FaithEngage.Core.Cards.Interfaces
 {
+    /// <summary>
+    /// Obtains RenderableCardDTOs from varous sources.
+    /// </summary>
     public interface ICardDTOFactory
     {
+        /// <summary>
+        /// Obtains an array of RenderableCrdDTOs from a dictionary of display
+        /// units, ordered by the integer key used.
+        /// </summary>
+        /// <returns>The cards.</returns>
+        /// <param name="units">A dictionary of DisplayUnits with integer keys indicating
+        /// their positions in the event.</param>
         RenderableCardDTO[] GetCards(Dictionary<int,DisplayUnit> units);
+        /// <summary>
+        /// Obtains a single card from a DisplayUnit.
+        /// </summary>
+        /// <returns>The card.</returns>
+        /// <param name="unit">Unit.</param>
         RenderableCardDTO GetCard (DisplayUnit unit);
-		RenderableCardDTO ConvertCard(IRenderableCard card);
+		/// <summary>
+        /// Converts an IRenderableCard to a RenderableCardDTO.
+        /// </summary>
+        /// <returns>The card.</returns>
+        /// <param name="card">Card.</param>
+        RenderableCardDTO ConvertCard(IRenderableCard card);
     }
 }
 
