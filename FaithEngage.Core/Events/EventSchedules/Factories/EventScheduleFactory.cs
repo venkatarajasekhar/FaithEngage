e7a4e49d9fd9f7198a043a8b71ca6f8e482038b7
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-21 16:26:01 -0500

    documenting EventScheduleFactory

--- FaithEngage.Core/Events/EventSchedules/Factories/EventScheduleFactory.cs
+++ FaithEngage.Core/Events/EventSchedules/Factories/EventScheduleFactory.cs
@@ -3,7 +3,10 @@ using FaithEngage.Core.Factories;
 
 namespace FaithEngage.Core.Events.EventSchedules.Factories
 {
-    public class EventScheduleFactory : IConverterFactory<EventScheduleDTO,EventSchedule>
+    /// <summary>
+    /// Converts and EventScheduleDTO to an EventSchedule. 
+    /// </summary>
+	public class EventScheduleFactory : IConverterFactory<EventScheduleDTO,EventSchedule>
 	{
 		
         public EventSchedule Convert (EventScheduleDTO dto)
@@ -15,18 +18,20 @@ namespace FaithEngage.Core.Events.EventSchedules.Factories
             sched.Id = dto.Id;
             sched.OrgId = dto.OrgId;
             sched.Recurrance = dto.Recurrance;
-
+			//Create the start time offset in UTC by adding a the utc start time to 
+			//epoch.
 			var startTime = new DateTime(0, DateTimeKind.Utc);
 			startTime = startTime.Add(dto.UTCStartTime);
+			//Create DateTimeOffset (aware of its offset from UTC)
 			var startDtOffset = new DateTimeOffset(startTime);
-
+			//Do the same for end time.
 			var endTime = new DateTime(0, DateTimeKind.Utc);
 			endTime = endTime.Add(dto.UTCEndTime);
 			var endDtOffset = new DateTimeOffset(endTime);
-
+			//Set the utc start and end times from the offsets.
 			sched.SetUTCStartTime(startDtOffset);
 			sched.SetUTCEndTime(endDtOffset);
-
+			//Set the recurrent start and end times.
 			sched.RecurringEnd = new DateTimeOffset(dto.UTCRecurringEnd, new TimeSpan());
 			sched.RecurringStart = new DateTimeOffset(dto.UTCRecurringStart, new TimeSpan());
 
