f28285b011cf7db8fb7a150d4e81b303ea750aa9
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-22 11:43:22 -0500

    documented and refactored eventdate

--- FaithEngage.Core/Events/Factories/EventFactory.cs
+++ FaithEngage.Core/Events/Factories/EventFactory.cs
@@ -6,18 +6,30 @@ using FaithEngage.Core.Factories;
 
 namespace FaithEngage.Core.Events.Factories
 {
+	/// <summary>
+	/// Converts an EventDTO to an Event
+	/// </summary>
 	public class EventFactory : IConverterFactory<EventDTO,Event>
 	{
         private readonly IEventScheduleRepoManager _schedRepoMgr;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:FaithEngage.Core.Events.Factories.EventFactory"/> class.
+		/// </summary>
+		/// <param name="schedRepoMgr">An IEventScheduleRepoManager</param>
 		public EventFactory(IEventScheduleRepoManager schedRepoMgr)
 		{
+			//Encapsulate the schedule repo manager.
 			_schedRepoMgr = schedRepoMgr;
 		}
+		/// <summary>
+		/// Converts an EventDTO to an Event
+		/// </summary>
+		/// <param name="dto">Dto.</param>
 		public Event Convert(EventDTO dto)
 		{
             var evnt = new Event ();
             evnt.AssociatedOrg = dto.AssociatedOrg;
-            evnt.EventDate = dto.EventDate;
+			evnt.EventDate = new DateTimeOffset (dto.UtcEventDate.Value, new TimeSpan ());
             evnt.EventId = dto.EventId;
             try {
                 evnt.Schedule = _schedRepoMgr.GetById (dto.EventScheduleId);;
