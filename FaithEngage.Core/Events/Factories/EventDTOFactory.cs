9494dd7959037e0ebd6c545980aba2f1ba6deb3f
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-22 11:42:40 -0500

    documented and refactored dto.eventdate

--- FaithEngage.Core/Events/Factories/EventDTOFactory.cs
+++ FaithEngage.Core/Events/Factories/EventDTOFactory.cs
@@ -3,13 +3,20 @@ using FaithEngage.Core.Factories;
 
 namespace FaithEngage.Core.Events.Factories
 {
+	/// <summary>
+	/// Converts an Event to an EventDTO
+	/// </summary>
 	public class EventDTOFactory : IConverterFactory<Event,EventDTO>
 	{
+		/// <summary>
+		/// Converts an Event to an EventDTO
+		/// </summary>
+		/// <param name="evnt">Evnt.</param>
 		public EventDTO Convert(Event evnt)
 		{
             var dto = new EventDTO ();
             dto.AssociatedOrg = evnt.AssociatedOrg;
-            dto.EventDate = evnt.EventDate;
+			dto.UtcEventDate = evnt.EventDate.Value.UtcDateTime;
             dto.EventId = evnt.EventId;
             dto.EventScheduleId = (evnt.Schedule != null) ? evnt.Schedule.Id : Guid.Empty;
             return dto;
