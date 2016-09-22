bfab9b9c02add5146b156bbd24c4486478f9aeb5
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-22 11:40:58 -0500

    documented and refactored eventdate

--- FaithEngage.Core/Events/EventDTO.cs
+++ FaithEngage.Core/Events/EventDTO.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 namespace FaithEngage.Core.Events
 {
+	/// <summary>
+	/// A data transfer object for Events.
+	/// </summary>
 	public class EventDTO
 	{
         public Guid EventId { get; set;}
         public Guid AssociatedOrg { get; set;}
         public Guid EventScheduleId { get; set;}
-        public DateTime? EventDate { get; set;}
+        public DateTime? UtcEventDate { get; set;}
 	}
 }
 
