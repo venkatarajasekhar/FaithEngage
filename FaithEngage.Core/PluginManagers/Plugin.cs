6d35a5494e3461a4221d64ff4b2e065afc101dab
jtfalkenstein <jtfalkenstein@gmail.com>
2016-09-29 19:45:46 -0500

    commented Plugin

--- FaithEngage.Core/PluginManagers/Plugin.cs
+++ FaithEngage.Core/PluginManagers/Plugin.cs
@@ -20,28 +20,57 @@ namespace FaithEngage.Core.PluginManagers
         /// <value>The plugin identifier.</value>
         public Guid? PluginId { get; set; }
 
+		/// <summary>
+		/// Gets the Plugin Type's full name, obtained through reflection.
+		/// </summary>
+		/// <value>The full name.</value>
         public string FullName
         {
             get{
                 return this.GetType ().FullName;
             }
         }
-
+		/// <summary>
+		/// Gets current assembly's location of the type, obtained through reflection.
+		/// </summary>
+		/// <value>The assembly location.</value>
         public virtual string AssemblyLocation{
             get{
                 return this.GetType ().Assembly.Location;
             }
         }
-
+		/// <summary>
+		/// Gets or sets the type of the plugin.
+		/// </summary>
+		/// <value>The type of the plugin.</value>
 		public PluginTypeEnum PluginType { get; set; }
-
+		/// <summary>
+		/// Gets the plugin version.
+		/// </summary>
+		/// <value>The plugin version.</value>
         abstract public int[] PluginVersion{ get;}
-
+		/// <summary>
+		/// Initializes the plugin, so that it can be used.
+		/// </summary>
+		/// <param name="FEFactory">FEF actory.</param>
         abstract public void Initialize (IAppFactory FEFactory);
+		/// <summary>
+		/// Installs the plugin.
+		/// </summary>
+		/// <param name="FEFactory">FEF actory.</param>
 		abstract public void Install (IAppFactory FEFactory);
+		/// <summary>
+		/// Uninstalls the plugin.
+		/// </summary>
+		/// <param name="FEFactory">FEF actory.</param>
 		abstract public void Uninstall (IAppFactory FEFactory);
+		/// <summary>
+		/// Registerss dependencies for the plugin, so that it can be initialized.
+		/// </summary>
+		/// <param name="regService">Reg service.</param>
 		abstract public void RegisterDependencies (IRegistrationService regService);
-        protected Plugin(){}
+        //TODO: This really should be removed. It doesn't really serve a purpose.
+		protected Plugin(){}
 
     }
 }
