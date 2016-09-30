32eeb5a235d6c784a3fe766a32f98a8abe263ace
jtfalkenstein <jtfalkenstein@gmail.com>
2016-09-29 19:33:11 -0500

    commenting IPluginManager

--- FaithEngage.Core/PluginManagers/Interfaces/IPluginManager.cs
+++ FaithEngage.Core/PluginManagers/Interfaces/IPluginManager.cs
@@ -7,14 +7,43 @@ using System.IO;
 
 namespace FaithEngage.Core.PluginManagers.Interfaces
 {
-    public interface IPluginManager
+    /// <summary>
+    /// Managers the installation, uninstallation, and initialization of plugins.
+    /// </summary>
+	public interface IPluginManager
     {
+		/// <summary>
+		/// Installs one or more plugins from a zip archive. 
+		/// </summary>
+		/// <param name="zipFile">The ziparchive containing any dlls, files, and the json manifest of plugins. </param>
 		int Install(ZipArchive zipFile);
-        void Install<TPlugin> (IList<FileInfo> files = null) where TPlugin : Plugin, new();
+        /// <summary>
+        /// Installs a plugin of the given type along with any associated files for the plugin.
+        /// </summary>
+        /// <param name="files">Files.</param>
+        /// <typeparam name="TPlugin">The Plugin Type</typeparam>
+		void Install<TPlugin> (IList<FileInfo> files = null) where TPlugin : Plugin, new();
+		/// <summary>
+		/// Uninstalls a plugin identified by the give id.
+		/// </summary>
+		/// <param name="pluginId">Plugin identifier.</param>
 		void Uninstall(Guid pluginId);
+		/// <summary>
+		/// Initializes all installed plugins (to be run at boot).
+		/// </summary>
 		void InitializeAllPlugins();
-        bool CheckRegistered (Guid pluginId);
-        bool CheckRegistered<TPlugin> () where TPlugin : Plugin;
+        /// <summary>
+        /// Checks whether the identified plugin has been registered.
+        /// </summary>
+        /// <returns><c>true</c>, if registered, <c>false</c> otherwise.</returns>
+        /// <param name="pluginId">Plugin identifier.</param>
+		bool CheckRegistered (Guid pluginId);
+        /// <summary>
+        /// Checks whether the identified plugin type has been registered.
+        /// </summary>
+        /// <returns><c>true</c>, if plugin has been registerd, <c>false</c> otherwise.</returns>
+        /// <typeparam name="TPlugin">The 1st type parameter.</typeparam>
+		bool CheckRegistered<TPlugin> () where TPlugin : Plugin;
     }
 }
 
